a23f8657472570b6bd7e549ca2f4b50354daec65
zacharyhampton <zhampton7131@dysartstudents.org>
2023-07-13 10:23:11 -0500

    - add sentry

--- harvester/config.py
+++ harvester/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import sentry_sdk
 
 load_dotenv()
 
@@ -7,6 +8,11 @@ if os.getenv("stage") == "dev":
     load_dotenv(dotenv_path=".env.dev")
 elif os.getenv("stage") == "prod":
     load_dotenv(dotenv_path=".env.prod")
+
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN"),
+        traces_sample_rate=0.1,
+    )
 
 MONGODB_URI = os.getenv("MONGODB_URI")
 REDIS_URL = os.getenv("REDIS_URI")
